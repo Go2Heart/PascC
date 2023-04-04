ddd6cd1652fb857ec1921c48cb66d0b25eec38f3
li-weihong <804400536@qq.com>
2023-04-05 00:31:41 +0800

    更新了命令行

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     parser = pparser.Parser()  # 语法分析器
     analyzer = Analyzer(symboltable, typestable)  # 语义分析器
     generator = CodeGenerater(symboltable, typestable)  # 代码生成器
-    with open("test/test1.pas", "r", encoding='utf-8') as f:
+    input = input("请输入测试文件名：")
+    with open("test/"+input+".pas", "r", encoding='utf-8') as f:
         node = parser.parse(f.read())  # 语法分析，得到抽象语法树根节点
         node.print()
         program = analyzer.analyse(node)  # 语义分析，得到语义语法树根节点
