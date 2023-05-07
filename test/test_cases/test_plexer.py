02a456a7beed279927451a58ac2ab0b595a759f0
LunqS <2315585087@qq.com>
2023-05-07 10:58:43 +0800

    词法分析单元测试完成

--- test/test_cases/test_plexer.py
+++ test/test_cases/test_plexer.py
@@ -1,61 +1,70 @@
 from src.lexical_syntactic.plexer import Lexer
 
 
-
 def test_lexer():
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex.pas')
-    a=test_lexer.token()
+    a = test_lexer.token()
     print(a)
     assert a is not None
 
-'''行长度超限'''
+
 def test_1():
-    test_lexer= Lexer()
+    '''行长度超限'''
+    test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_1.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_1.out', 'w', encoding='utf-8'))
 
-'''标识符过长'''
+
 def test_2():
+    '''标识符过长'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_2.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_2.out', 'w', encoding='utf-8'))
 
-'''非法字符'''
+
 def test_3():
+    '''非法字符'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_3.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_3.out', 'w', encoding='utf-8'))
 
-'''读取字符常量时遇到文件尾EOF'''
+
 def test_4():
+    '''读取字符常量时遇到文件尾EOF'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_4.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_4.out', 'w', encoding='utf-8'))
 
-'''读取的字符常量为空'''
+
 def test_5():
+    '''读取的字符常量为空'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_5.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_5.out', 'w', encoding='utf-8'))
 
-'''读取的字符常量不只一个字符'''
+
 def test_6():
+    '''读取的字符常量不只一个字符'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_6.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_6.out', 'w', encoding='utf-8'))
 
-'''读取字符常量先遇到换行符而不是单引号'''
+
 def test_7():
+    '''读取字符常量先遇到换行符而不是单引号'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_7.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_7.out', 'w', encoding='utf-8'))
 
-'''读取多行注释时遇到文件尾'''
+
 def test_8():
+    '''读取多行注释时遇到文件尾'''
     test_lexer = Lexer()
     test_lexer.load_file('./test_lex/test_lex_8.pas')
     test_lexer.scan(output_file=open('./test_lex/test_lex_8.out', 'w', encoding='utf-8'))
 
+
 def test_c1():
+
     pass
