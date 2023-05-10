e4b13581dbe6cf52899d1164fe34a4bef567a4f5
Go2Heart <Go2Heart@outlook.com>
2023-05-10 16:48:28 +0800

    [Fix] Runnable Test

--- test/addons.pas
+++ test/addons.pas
@@ -7,27 +7,31 @@ type
     GPA: real;
   end;
 
+  TCourse = record
+    Name: string;
+    Code: string;
+    Credit: integer;
+  end;
+
+  TEnrollment = record
+    Student: TStudent;
+    Course: TCourse;
+    Grade: integer;
+  end;
+
 var
-  Student1: TStudent;
-  Student2: TStudent;
+  Enrollment: TEnrollment;
 
 begin
-  Student1.Name := 'Alice';
-  Student1.Age := 20;
-  Student1.GPA := 3.8;
+  Enrollment.Student.Name := 'Alice';
+  Enrollment.Student.Age := 20;
+  Enrollment.Student.GPA := 3.5;
+  Enrollment.Course.Name := 'Programming 101';
+  Enrollment.Course.Code := 'CS101';
+  Enrollment.Course.Credit := 3;
+  Enrollment.Grade := 85;
 
-  Student2.Name := 'Bob';
-  Student2.Age := 22;
-  Student2.GPA := 3.5;
-
-  writeln('Student 1:');
-  writeln('Name: ', Student1.Name);
-  writeln('Age: ', Student1.Age);
-  writeln('GPA: ', Student1.GPA:4:2);
-
-  writeln('Student 2:');
-  writeln('Name: ', Student2.Name);
-  writeln('Age: ', Student2.Age);
-  writeln('GPA: ', Student2.GPA:4:2);
-
+  writeln('Student: ', Enrollment.Student.Name);
+  writeln('Course: ', Enrollment.Course.Name);
+  writeln('Grade: ', Enrollment.Grade);
 end.
